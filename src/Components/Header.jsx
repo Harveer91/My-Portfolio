9934ad3e9c0027d1012483ace26bc7f879d3c55a
Harveer Dhillon <harveerdhillon91@gmail.com>
2025-07-31 20:13:06 -0400

    Centered the second line on the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,10 @@ function Header(){
         <header>
             <div className="header">
                 <h1 className="Introduction" id="Name">Hi I'm Harveer</h1>
-                <div className="typing-container">
-                    <h1 className="Introduction" id="Occupation">A Software Engineer</h1>
+                <div className="typing-wrapper">
+                    <div className="typing-container">
+                        <h1 className="Introduction" id="Occupation">A Software Engineer</h1>
+                    </div>
                 </div>
             </div>
         </header>
