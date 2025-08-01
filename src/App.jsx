b768d67480a47f51051ca58f3ebb1ee8abe335d1
Harveer Dhillon <harveerdhillon91@gmail.com>
2025-08-01 02:25:45 -0400

    Added about me prototype section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import './index.css'
 import Header from "./Components/Header"
 import Button from './Components/Button';
+import AboutMe from './Components/AboutMe';
 
 function App() {
   return (
     <>
-      <Header/>
-      <Button/>
-      <hr id="line2"></hr>
-      <hr id="line"></hr>
+      <main>
+        <section className="page-selection" id="Home"></section>
+          <Header/>
+          <Button/>
+          <hr id="line2"></hr>
+          <hr id="line"></hr>
+        <section className="page-selection" id="About"></section>
+          <AboutMe/>
+        <section className="page-selection" id="Skills"></section>
+        <section className="page-selection" id="Projects"></section>
+      </main>
     </>
   );
 }
