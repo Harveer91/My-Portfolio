2a78d7dbec42e27a2fab8bd04e6ea89d968e6a44
Harveer Dhillon <harveerdhillon91@gmail.com>
2025-08-01 03:20:21 -0400

    Finished the about me page/section

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,10 +1,14 @@
 function AboutMe(){
     return(
-        <div className='textbox' id='aboutme'>
-            <h1>About Me</h1>
-            <p1>I'm an ambitious software engineer who is driven by the need of constant self-improvement. I am passionate about full stack development and machine learning, where I can design frontend to backend solutions for users and, explore the realm of data driven decision making. Due to my unique background, I thrive in collaborative team environments, applying logic and analytical thinking when solving problems and have to ability to utilize my strong technical skills to create impactful applications.</p1>
-        </div>
-
+        <>
+            <h1 id="a-title">About Me</h1>
+            <div className='textbox' id='aboutme'>
+                <p1>I'm an ambitious software engineer who is driven by the need of constant self-improvement. I am passionate about full stack development and machine learning, where I can design frontend to backend solutions for users and, explore the realm of data driven decision making. Due to my unique background, I thrive in collaborative team environments, applying logic and analytical thinking when solving problems and have to ability to utilize my strong technical skills to create impactful applications.</p1>
+            </div>
+            <div id="profile-image">
+                <img src="/src/assets/profile.jpg" alt="Profile"></img>
+            </div>
+        </>
     )
 }
 
